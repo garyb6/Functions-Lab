e87014f1efe95d2d3f9c7e817a222ab888cf7c29
garyb6 <gary.bennett6@outlook.com>
2021-11-17 13:54:53 +0000

    adding functions_lab_1_testing

---
+++ src/python_functions_practice.py
@@ -0,0 +1,33 @@
+def return_10(): 
+    return 10 
+
+
+
+def add(n1, n2):
+    return (1+2)
+
+def subtract(n1, n2):
+    return (10-5)
+
+def multiply(n1, n2):
+    return (4*2)
+
+def divide(p1, p2):
+    return (10/2)
+
+# def length_of_string (banana):
+#     test_string = "A string of length 21"
+#     #return int(test_string)
+#     length= []
+#     for string_length in test_string():
+#         if string_length.isdigit():
+#             length.append(int(test_string))
+#     return int(length)
+
+def length_of_string (string):
+    return len(string)
+
+def join_string (string1, string2):
+    return (string1+string2)
+
+
